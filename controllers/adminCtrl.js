56308fb12304cacea68b1a43685cc32ab51490e2
cheonHwi <cheonhwi@lyuchaeyeon-ui-MacBookPro.local>
2022-10-02 20:50:20 +0900

    asd

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -22,7 +22,7 @@ const lookupVisitor = (dateInfo) => {
     return new Promise((resolve, reject) =>{
         const { lookupYear, lookupMonth } = dateInfo
         const query = {entranceYear: {$eq : lookupYear}, entranceMonth: { $eq: lookupMonth }}
-        const readField = {visitorName:1,visitorPhoneNumber:1,visitorReason:1,_id:1, entranceMonth:1, entranceDay:1, pinNumber:1};
+        const readField = {visitorName:1,visitorPhoneNumber:1,visitorReason:1,_id:1, entranceMonth:1, entranceDay:1, pinNumber:1 };
         visitorModel.find(query, readField).sort({_id:-1})
         .then(result => {
             if(result !== null){
